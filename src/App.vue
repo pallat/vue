f462bbd13fc259322b216c3c21383f6e23048257
pallat <yod.pallat@gmail.com>
2017-09-13 11:21:17 +0700

    write component

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,13 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js Appppppp'
-    }
-  }
+<<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class App extends Vue {
+  msg: string = 'Welcome to Your Vue.js App'
 }
 </script>
 
